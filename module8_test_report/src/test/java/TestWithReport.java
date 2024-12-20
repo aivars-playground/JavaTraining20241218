50cfa3bfaa287fa704d4930f447339220620d244
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-20 21:01:12 +0000

    decimal values

--- module8_test_report/src/test/java/TestWithReport.java
+++ module8_test_report/src/test/java/TestWithReport.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("this test should:")  //does not seem to work with current surefire
@@ -63,6 +67,40 @@ public class TestWithReport {
         @DisplayName("run another nested test")
         void anotherTestWithNested() {
             System.out.println("nested test 2");
+        }
+
+        @Test
+        void anotherDecimal() {
+            {
+                double d1 = 1.15d;
+                double d2 = 1.05d;
+                System.out.println("float d1-d2: " + (d1 - d2));
+
+
+                float f1 = 1.15f;
+                float f2 = 1.05f;
+                System.out.println("float f1-f2: " + (f1 - f2));
+                System.out.println("new BigDecimal f1-f2: " +(new BigDecimal(f1)).subtract(new BigDecimal(f2)));
+                System.out.println("BigDecimal.of f1-f2: " +(BigDecimal.valueOf(f1)).subtract(BigDecimal.valueOf(f2)));
+                System.out.println("new BigDecimal string... f1-f2: " +(new BigDecimal(String.valueOf(f1))).subtract(new BigDecimal(String.valueOf(f2))));
+
+                System.out.println("new BigDecimal with context f1-f2: " +(new BigDecimal(f1, new MathContext(3))).subtract(new BigDecimal(f2,new MathContext(3))));
+
+
+                System.out.println("string f1" + String.valueOf(f1));
+                System.out.println("BigDecimal.of scaled f1 in pennies -f2 in pennies: " +(BigDecimal.valueOf(115,2)).subtract(BigDecimal.valueOf(105,2)));
+
+                System.out.println("100/3 = with scale :" + BigDecimal.valueOf(100,2).divide(BigDecimal.valueOf(3), 3, RoundingMode.HALF_UP));
+
+                System.out.println("float 0.1333333" + BigDecimal.valueOf(0.1333333));
+                System.out.println("float 0.1333333, scale 2 --- " + BigDecimal.valueOf(0.1333333).setScale(2, RoundingMode.HALF_UP));
+                System.out.println("float 0.1333333, precision 3 --- " + BigDecimal.valueOf(1.15d).subtract(BigDecimal.valueOf(1.05d)));
+                System.out.println("float 0.1333333, precision 3 --- " + BigDecimal.valueOf(1.15f).subtract(BigDecimal.valueOf(1.05f)));
+                System.out.println("float 0.1333333, precision 3 --- " + BigDecimal.valueOf(1.15).subtract(BigDecimal.valueOf(1.05)));
+
+
+
+            }
         }
     }
 }
