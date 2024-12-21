c4baa463ac7aabdd49a2ca428002fc61f5f8bc3d
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-21 22:04:34 +0000

    string transformations

--- module9_misc/src/test/java/StringTest.java
+++ module9_misc/src/test/java/StringTest.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.function.Predicate.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -15,12 +22,12 @@ public class StringTest {
     void test_trimVsStrip() {
 
         String testStrWithEmspace = "Hello World\u2003";
-        System.out.println("`"+testStrWithEmspace+"`");
+        System.out.println("`" + testStrWithEmspace + "`");
 
         //trim ignores some unicode whitespaces
-        assertNotEquals("Hello World" , testStrWithEmspace.trim());
+        assertNotEquals("Hello World", testStrWithEmspace.trim());
 
-        assertNotEquals("Hello World" , testStrWithEmspace.strip());
+        assertNotEquals("Hello World", testStrWithEmspace.strip());
 
 
     }
@@ -30,16 +37,16 @@ public class StringTest {
 
         String notEmpty = "Hello World";
 
-        System.out.println("notEmpty:isEmpty:" + notEmpty.isEmpty() + " isBlank:" + notEmpty.isBlank() );
+        System.out.println("notEmpty:isEmpty:" + notEmpty.isEmpty() + " isBlank:" + notEmpty.isBlank());
 
         String empty = "";
-        System.out.println("notEmpty:isEmpty:" + empty.isEmpty() + " isBlank:" + empty.isBlank() );
+        System.out.println("notEmpty:isEmpty:" + empty.isEmpty() + " isBlank:" + empty.isBlank());
 
         String spaces = "  ";
-        System.out.println("spaces:isEmpty:" + spaces.isEmpty() + " isBlank:" + spaces.isBlank() );
+        System.out.println("spaces:isEmpty:" + spaces.isEmpty() + " isBlank:" + spaces.isBlank());
 
         String emspace = "\u2003";
-        System.out.println("emspace:isEmpty:" + emspace.isEmpty() + " isBlank:" + emspace.isBlank() );
+        System.out.println("emspace:isEmpty:" + emspace.isEmpty() + " isBlank:" + emspace.isBlank());
     }
 
     @Test
@@ -47,8 +54,8 @@ public class StringTest {
 
         String testStrWithCurrency = " 1000 usd ";
 
-        //String::replaceAll does not work in java 23
         UnaryOperator<String> removeLetters = (txt) -> Pattern.compile("[A-Z]").matcher(txt).replaceAll("");
+
 
         UnaryOperator<String> usNumber = (txt) -> NumberFormat.getNumberInstance(Locale.US).format(Integer.valueOf(txt));
 
@@ -59,16 +66,59 @@ public class StringTest {
                 .transform(usNumber)
                 .concat("$");
 
-        assertEquals("1,000$",res);
-
+        assertEquals("1,000$", res);
 
         String res2 = testStrWithCurrency
                 .transform(String::toUpperCase)
-                .transform(removeLetters)
+                .replaceAll("[A-Z]", "")
                 .transform(String::strip)
                 .transform(usNumber)
                 .concat("$");
 
-        assertEquals(res2,res);
+        assertEquals(res2, res);
+    }
+
+    @Test
+    void test_matches() {
+
+        var data = "Java 23";
+
+        System.out.println(data.matches("Java 23"));    //true
+        System.out.println(data.matches("Java"));       //false not exact match
+        System.out.println(data.matches("java 23"));    //false not exact match
+
+        System.out.println(data.matches("[jJ]ava.*"));      //true (any number of chars)
+        System.out.println(data.matches("[jJ]ava [0-9]+")); //true (ar least one digit)0
+    }
+
+    @Test
+    void test_lines() {
+
+        String testStr = """
+                address line 1
+                address line 2
+                address line 3
+                post code (with padding)
+                """;
+
+        Stream<String> lines = testStr.lines();
+
+        AtomicInteger count = new AtomicInteger(1);
+
+        lines.forEach(line -> System.out.println(count.getAndIncrement() + ": '" + line + "'"));
+    }
+
+    @Test
+    void test_split_lines() {
+        var address = "1,address line 1,2,address line 2,3,address line 3";
+        var withNewLine = address.replaceAll(",([0-9]+),","\n$1,");
+        withNewLine.lines().forEach(System.out::println);
+
+        var back = withNewLine.lines().toList().stream()
+                .filter(Objects::nonNull)
+                .filter(not(String::isBlank))
+                .collect(Collectors.joining(";","[","]"));
+
+        System.out.println("in :"+address+ "\nout:" + back);
     }
 }
