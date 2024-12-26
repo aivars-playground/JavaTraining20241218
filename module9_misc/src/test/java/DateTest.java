d804aa54aab411f6ed0cd0693806fdc8864e1fa1
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2024-12-26 12:51:57 +0000

    date transformations

--- module9_misc/src/test/java/DateTest.java
+++ module9_misc/src/test/java/DateTest.java
@@ -64,4 +64,72 @@ public class DateTest {
 
     }
 
+    @Test
+    public void dateInstantZonedLocal() {
+        System.out.println(Instant.now());
+        System.out.println(LocalDateTime.now());
+
+        System.out.println(ZoneId.getAvailableZoneIds());
+        System.out.println("Rarotonga time:"+LocalDateTime.now(ZoneId.of("Pacific/Rarotonga")));
+
+
+        LocalTime timeNow = LocalTime.now();
+        LocalDate dateNow = LocalDate.now();
+        LocalDateTime now = LocalDateTime.of(dateNow, timeNow);
+        System.out.println("now:" +now);
+
+        LocalDateTime timeMin = LocalDateTime.of(dateNow, LocalTime.MIN);
+        LocalDateTime timeMidnight = LocalDateTime.of(dateNow, LocalTime.MIDNIGHT);
+        LocalDateTime timeNOON = LocalDateTime.of(dateNow, LocalTime.NOON);
+        LocalDateTime timeMax = LocalDateTime.of(dateNow, LocalTime.MAX);
+
+        System.out.println(
+                timeMin + " timeMin:\n" +
+                timeMidnight + " timeMidnight:\n" +
+                timeNOON + " timeNOON:\n" +
+                timeMax+ " timeMax:\n"
+        );
+    }
+
+    @Test
+    public void dateZones() {
+
+        var scheduledDepature = LocalDateTime.of(2025,1,1,6, 30);
+        var arrival = scheduledDepature.plusHours(6);
+
+        var departLondonTime = scheduledDepature.atZone(ZoneId.of("Europe/London"));
+        var arriveLondonTime = arrival.atZone(ZoneId.of("Europe/London"));
+
+        var arriveNyTIme = arriveLondonTime.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+        System.out.println("depart from London, local time:"+ departLondonTime);
+        System.out.println("arrive at New_York, local time:"+ arriveNyTIme);
+
+        System.out.println("arrive at New_York, London time:"+ arriveLondonTime);
+    }
+
+    @Test
+    public void dateFlightRecord() {
+
+        record Flight(String from, String to, LocalDateTime departure, int duration) {
+            @Override
+            public String toString() {
+                ZoneId fromZone = ZoneId.of(from);
+                ZoneId toZone = ZoneId.of(to);
+
+                var departTime = departure.atZone(fromZone);
+                var arrivalTime =departure.plusHours(duration)
+                        .atZone(fromZone)
+                        .withZoneSameInstant(toZone);
+
+                return String.format("Flight departs at %s, and arrives at %s", departTime, arrivalTime);
+            }
+        }
+
+        System.out.println("Flights:");
+        var departureLondonToNy = LocalDateTime.of(2025,1,1,6, 30);
+        System.out.println("leg1:"+new Flight("Europe/London", "America/New_York", departureLondonToNy,8));
+        var departureNyToYakutat = LocalDateTime.of(2025,1,1,12, 0);
+        System.out.println("leg2:"+new Flight("America/New_York", "America/Yakutat",departureNyToYakutat,4));
+    }
 }
