4109be73af943b1d35146a058b9f546e49e4b606
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-01-15 21:14:48 +0000

    float fun

--- module9_misc/src/test/java/Overflow.java
+++ module9_misc/src/test/java/Overflow.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Overflow {
 
@@ -52,6 +54,18 @@ public class Overflow {
         System.out.println("o.3 - o.11 float:" + (o3 - o1) + " o2:" +o2 );
 
         System.out.println("(o.3 - o.1) == o.2 :" + ((o3 - o1) == o2) );
+
+        System.out.println(" ==" + (BigDecimal.valueOf(0.3f).subtract(BigDecimal.valueOf(0.1f))).toPlainString() );
+        System.out.println(" ==" + (BigDecimal.valueOf(3,1).subtract(BigDecimal.valueOf(1,1))).toPlainString() );
+        System.out.println(" ==" + (BigDecimal.valueOf(3,1).
+                divide(BigDecimal.valueOf(7,0), new MathContext(3, RoundingMode.HALF_EVEN)).toPlainString() ));
+
+        System.out.println(" ==" + (BigDecimal.valueOf(3,1).
+                divide(BigDecimal.valueOf(7,0), MathContext.DECIMAL32).setScale(3, RoundingMode.HALF_EVEN) ).toPlainString() );
+
+        System.out.println(" ==" + (BigDecimal.valueOf(300,0).
+                multiply(BigDecimal.valueOf(7,0), new MathContext(0, RoundingMode.HALF_EVEN)).toPlainString() ));
+
 
     }
 
