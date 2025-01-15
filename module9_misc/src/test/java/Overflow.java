be6a78515de546ed7a8f20427230515ae50e75a2
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-01-15 20:21:17 +0000

    float fun

--- module9_misc/src/test/java/Overflow.java
+++ module9_misc/src/test/java/Overflow.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 public class Overflow {
 
 
@@ -32,6 +34,24 @@ public class Overflow {
 
         System.out.println("7 >>> 1 = " + (seven >>> 1));
         System.out.println("-7 >>> 1 = " + ((-seven) >>> 1) ); //2147483644 ??????
+
+    }
+
+    @Test
+    void test_float() {
+        System.out.println("Max int:" + Integer.MAX_VALUE );
+        System.out.println("Max flt:" + BigDecimal.valueOf(Float.MAX_VALUE).toPlainString() );
+
+        float maxI2F = Integer.MAX_VALUE;
+        System.out.println("Max int to F:" + maxI2F );
+        System.out.println("Max maxI2F:" + BigDecimal.valueOf(maxI2F).toPlainString() );
+
+        float o1 = 0.1f;
+        float o3 =  0.3f;
+        float o2 =  0.2f;
+        System.out.println("o.3 - o.11 float:" + (o3 - o1) + " o2:" +o2 );
+
+        System.out.println("(o.3 - o.1) == o.2 :" + ((o3 - o1) == o2) );
 
     }
 
