c2bc03f5ed8cd4f9b2e3500a346d6a3a89fcda87
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-01-18 20:10:11 +0000

    thread fun

--- module11_concurrency/src/test/java/C01_RunnableTest.java
+++ module11_concurrency/src/test/java/C01_RunnableTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-public class RunnableTest {
+public class C01_RunnableTest {
 
     @Test
     void threads_joinfirst() throws InterruptedException {
@@ -122,5 +122,56 @@ public class RunnableTest {
         thread.start();
         thread.interrupt();
         thread.join();
+    }
+
+    @Test
+    void delayed_start() throws InterruptedException {
+        class Sleepy implements Runnable {
+            private final String name;
+            public Sleepy(String name) {
+                this.name = name;
+            }
+            @Override
+            public void run() {
+                for (int i=0; i<10;i++) {
+                    System.out.println("Sleepy:"+name+" i->"+i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        Thread sleepy = new Thread(new Sleepy("first"));
+        Thread sleepy2 = new Thread(new Sleepy("looser"));
+
+        sleepy.start();
+        sleepy.join(1500);
+        sleepy2.start();
+        sleepy2.join();
+    }
+
+    @Test
+    void locks_really_bad_example() throws InterruptedException {
+
+        class Count {
+            public static int count = 0;
+
+            public static void increment() {
+                int current = count;
+                System.out.println("Before count->"+count +" @thread:"+Thread.currentThread().getName()); // reduced by 5... overwrites
+                count = current + 1;
+                System.out.println("After count->"+count +" @thread:"+Thread.currentThread().getName());  // reduced by 8 .. exits rtoo early
+            }
+        }
+
+        for(int i=0;i<10;i++) {
+            Thread t = new Thread( () -> Count.increment());
+            t.start();
+        }
+
+        System.out.println("c->"+Count.count);
     }
 }
