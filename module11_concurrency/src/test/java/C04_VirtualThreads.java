1b11ef307187c853d4966be86bfc3ed6ab07830c
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-01-23 20:47:14 +0000

    thread fun

--- module11_concurrency/src/test/java/C04_VirtualThreads.java
+++ module11_concurrency/src/test/java/C04_VirtualThreads.java
@@ -2,23 +2,24 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 public class C04_VirtualThreads {
+
+    Runnable runnable = () -> {
+        try {
+            //System.out.println("sleep start:" + Thread.currentThread().getName());
+            Thread.sleep(1000);
+            //System.out.println("sleep stop:" + Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    };
 
     @Test
     void testSystemThread() throws InterruptedException {
 
         long start = System.currentTimeMillis();
-
-        Runnable runnable = () -> {
-            try {
-                //System.out.println("sleep start:" + Thread.currentThread().getName());
-                Thread.sleep(1000);
-                //System.out.println("sleep stop:" + Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        };
 
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 1_000_000; i++) {
@@ -42,16 +43,6 @@ public class C04_VirtualThreads {
 
         long start = System.currentTimeMillis();
 
-        Runnable runnable = () -> {
-            try {
-                //System.out.println("sleep start:" + Thread.currentThread().getName());
-                Thread.sleep(1000);
-                //System.out.println("sleep stop:" + Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        };
-
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 1_000_000; i++) {
             Thread t = Thread.ofVirtual().start(runnable);
@@ -69,4 +60,19 @@ public class C04_VirtualThreads {
     }
 
 
+    @Test
+    void testVirtualThreadExecutor() throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+
+        try (var es = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 1_000_000; i++) {
+                es.submit(runnable);
+            }
+        }
+
+        long stop = System.currentTimeMillis();
+        System.out.println("System thread time: " + (stop - start)  );
+
+    }
 }
