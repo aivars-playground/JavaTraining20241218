855f483c44b1c2ae577cf55b888b831c6469541c
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-01-29 01:00:07 +0000

    various async tasks

---
+++ module11_concurrency/src/test/java/C07_ChainFutures.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class C07_ChainFutures {
+
+    @Test
+    public void tasks_chaining() {
+
+        record User(Long id, String name) {}
+
+        Function<List<Long>, List<User>> readUsers =
+                longs -> longs.stream().map(l -> new User(l, "user " + l)).collect(Collectors.toList());
+
+        CompletableFuture<List<User>> future =
+                CompletableFuture.supplyAsync(() -> List.of(1L, 2L, 3L))
+                        .thenApply(longs -> readUsers.apply(longs));
+
+        future.thenRun(() -> System.out.println("Done") );
+
+        future.thenAccept(users -> System.out.println("users size:"+users.size()));
+    }
+
+}
